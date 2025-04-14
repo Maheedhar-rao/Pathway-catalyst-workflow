9dd50b3683ce892b04804cb382de96814888df2c
Maheedhar rao Govada <106291633+Maheedhar-rao@users.noreply.github.com>
2025-04-14 17:57:13 -0400

    Update server.js
    Changed the serving page to index.html

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ const lenderEmailMap = (() => {
 app.use(cors());
 app.use(bodyParser.json({ limit: '120mb' }));
 app.use(bodyParser.urlencoded({ limit: '120mb', extended: true }));
-app.use(express.static(path.join(__dirname, '../Frontend', 'login.html')));
+app.use(express.static(path.join(__dirname, '../Frontend', 'index.html')));
 
 app.use('/api', signupRoutes);
 
