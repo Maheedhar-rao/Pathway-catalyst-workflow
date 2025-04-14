387eaade12b2197d8092ca2ab6cfce3c4c259d32
Maheedhar rao Govada <106291633+Maheedhar-rao@users.noreply.github.com>
2025-04-14 18:32:39 -0400

    Update login.js
    SUPABASE_KEY updated

--- server/login.js
+++ server/login.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { createClient } = require('@supabase/supabase-js');
 const bcrypt = require('bcrypt');
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
 router.post('/api/login', async (req, res) => {
   const { email, password } = req.body;
